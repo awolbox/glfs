a3856182ca0e264d773c4019d1005cce8b60efed
awolbox <awol@member.fsf.org>
2018-05-06 00:39:06 -0400

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,49 +1,61 @@
 #!/bin/bash
 
 executive='makesrc.sh'
-readonly torn_executive="$(printf '%s\n' "${executive}" | cut -d "." -f 1)"
+readonly torn_executive="$(echo "${executive}" | cut -d "." -f 1)"
 readonly script=".${torn_executive}"
 
 # Native files handled
 _vars()
 {
 		# Start if 'vars' exists
-		touch -v "${script}" && chmod 600 "${script}" || return 1
+		touch ${script} || return 1
 				
 		# If $torn_executive exists, cat 'vars' to $script, thus starting "compilation"
-		cat 'vars' > "${script}" && rm -v 'vars' || return 1
+		cat vars > ${script} && rm vars || return 1
 }
 
 _functions()
 {
 		local line
 		
-		while read -r line
+		while read line
 		do
-				cat $(cat ${line} | cut -d '"' -f 2) >> ${script} || return 1
+				cat $(cut -d '"' -f 2) >> ${script}
 		done < 'functions'
 
-		cat "${torn_executive}" >> "${script}" && rm -v "${torn_executive}" || return 1
+		if [ $? -eq 0 ]
+		then
+				{ cat ${torn_executive} >> ${script} && rm -v ${torn_executive}; } || return 1
+		else
+				return 1
+		fi
 }
 
 # Return values handled
 _returns()
 {
 		ret=$?
-		while [ $? -ne 0 ]
-		do
+		if [ "$ret" -ne 0 ]
+		then
 				case "$ret" in 
-						'1') exit 1; break ;;
+						'1') exit 1 ;;
 				esac
-		done
+		fi
+}
+
+# Special files handled
+#special=()
+_special()
+{
+		:
 }
 
 # Clean up
 _sweep()
 {
-		mv -v ${script} ${executive}
-		chmod 700 ${executive}
-		rm -v ./*_
+		mv ${script} ${executive}
+		chmod +x ${executive}
+		rm -v ./_*
 }
 
 # Check current working directory for special files
@@ -53,8 +65,11 @@ do
 		case "$file" in
 				'vars') _vars; _returns ;;
 				'functions') _functions; _returns ;;
-				"${special[@]}") : ;;
-		esac; _finish
+				#"${special[@]}") : ;;
+		esac
 done
+
+[ $? -eq 0 ] && { _sweep; }
 exit $?
+
 
